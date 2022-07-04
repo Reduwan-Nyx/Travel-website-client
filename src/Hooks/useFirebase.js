1362b308f495e4a267423eda1520242e8dfffe6f
Tahsim Reduwan Naim <88936596+Reduwan-Nyx@users.noreply.github.com>
2022-07-04 20:33:56 +0600

    footer completed

---
+++ src/Hooks/useFirebase.js
@@ -0,0 +1,68 @@
+import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signOut,
+  } from "firebase/auth";
+  import { useEffect, useState } from "react";
+  import initializeAuthentication from "../Pages/Login/Firebase/firebase.init";
+  
+  initializeAuthentication();
+  
+  const useFirebase = () => {
+    const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+  
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+  
+    const signInUsingGoogle = () => {
+      setIsLoading(true);
+  
+      return signInWithPopup(auth, googleProvider)
+      .finally(() =>
+      setIsLoading(false)
+      );
+    }
+  
+  
+    // observe user state change
+    useEffect(() => {
+      const unsubscribed = onAuthStateChanged(auth, (user) => {
+          console.log(user);
+        if (user) {
+          setUser(user);
+        } else {
+          setUser({});
+        }
+        setIsLoading(false);
+      });
+      return () => unsubscribed;
+    }, []);
+  
+    //   Log Out
+    const logOut = () => {
+      setIsLoading(true);
+      signOut(auth)
+      .then(() => {
+          setUser({});
+        })
+        .finally(() => setIsLoading(false));
+        // .catch((error) => {
+        //   // An error happened.
+          
+        // });
+    };
+  
+    return {
+      user,
+      isLoading,
+      setUser,
+      signInUsingGoogle,
+      logOut,
+    };
+  };
+  
+  export default useFirebase;
+  
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Services from '../Services/Services';
+import Footer from '../Shared/Footer/Footer';
 import About from './About/About';
 import Banner from './Banner/Banner';
 import Features from './Features/Features';
+import Testimonials from './Testimonials/Testimonials';
 import Tickets from './Tickets/Tickets';
 
 const Home = () => {
@@ -13,6 +15,8 @@ const Home = () => {
             <About></About>
             <Tickets></Tickets>
             <Services></Services>
+            <Testimonials></Testimonials>
+            <Footer></Footer>
         </div>
     );
 };
